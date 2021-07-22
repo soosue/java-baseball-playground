25014bfcf0a6156037a97047b56f6ed644aa71dd
soo <yunsoo0510@daum.net>
2021-07-22 23:07:32 +0900

    String과 Set에 대한 요구사항에 따른 테스트코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,49 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        String[] actual2 = "1,".split(",");
+
+        assertThat(actual).contains("1", "2");
+        assertThat(actual2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String abc = "abc";
+
+        assertThat(abc.charAt(0)).isEqualTo('a');
+        assertThat(abc.charAt(1)).isEqualTo('b');
+        assertThat(abc.charAt(2)).isEqualTo('c');
+    }
+
+    @DisplayName("charAt메소드 인덱스 벗어난 입력에 대한 예외 테스트")
+    @Test
+    void charAtIndexOutOfBound() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("4");
     }
 }
